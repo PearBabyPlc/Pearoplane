3c2a1024b4c9265a23add2c956c92fbdf0509531
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2026-06-08 18:22:34 +1000

    Update display.cpp

--- display/display.cpp
+++ display/display.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <cmath>
-#include <string>
 #include <ctime>
 using namespace std;
 
-// display.cpp is set up as a main file with the function below to allow for execution
-// and debugging in the console, just on its own. as the program is written it'll be
-// condensed down so there's only one int main() in the whole project. cmake stuff 
+// this is probably gonna be the starting point for the entire main program
+// in the meantime display.cpp will just be a testbed for user input and 3d rendering of the aircraft.obj file
+// physics.cpp will be integrated in as a header file or something
+// i want this flight sim to be as simple as possible and to consolidate memory usage as much as possible
+// this may end up being impractical with the 3d rendering openGL 4.1 stuff though
+// so these two segments of the sim will be kept separate until it's time to put it all together
 
 int main() {
 	clock_t before = clock();
