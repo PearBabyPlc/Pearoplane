135de09c50858ec48aa39f625d8bd177ed2e3c89
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2026-06-09 00:58:00 +1000

    Update formulaegg.h

--- aero/formulaegg.h
+++ aero/formulaegg.h
@@ -48,7 +48,81 @@ namespace isen {
 namespace normal {
 }
 
+const double degInRad = 180 / pi;
+const double radInDeg = pi / 180;
+void degToRad(float &angle) {
+	angle *= radInDeg;
+}
+void radToDeg(float &angle) {
+	angle *= degInRad;
+}
+
 namespace oblique {
+	float radMachAngle(Station &sta) {
+		return asin(1 / sta.M);
+	}
+	float radDeflectionAngle(Station &sta, float S) {
+		double sinS = sin(S);
+		double invCota = 1 / (tan(S) * ((((sta.gam + 1) * pow(sta.M, 2) / (2 * (pow(sta.M, 2) * pow(sinS, 2) - 1))) - 1)));
+		return atan(invCota);
+	}
+	float maxDefRad(Station &sta) {
+		double onePointFive = pow(sta.M, 2) - 1;
+		return (4 / (3 * sqrt(3) * (sta.gam + 1))) * (pow(onePointFive, 1.5) / pow(sta.M, 2));
+	}
+	float radShockAngle(Station &sta, float A) {
+		int minAngleDeg = floor(degInRad * radMachAngle(sta));
+		float Adeg = degInRad * A;
+		float wipA;
+		float &wipAdeg = wipA;
+		float iNext;
+		float &iTarget = iNext;
+		for (int i = minAngleDeg; i < 91; i++) {
+			float degi = radInDeg * i;
+			float tempi = radDeflectionAngle(sta, degi);
+			wipAdeg = degInRad * tempi;
+			iTarget = i;
+			//std::cout << "\n s=" << i << " a=" << wipAdeg << " targ=" << Adeg << " diff=" << abs(Adeg - wipAdeg); debug
+			if (abs(Adeg - wipAdeg) <= 1.0) {
+				break;
+			}
+		}
+		float Sdeg;
+		float &wipS = Sdeg;
+		for (int i = 0; i < 2001; i++) {
+			wipS = iNext + (0.0005 * i);
+			float degi = radInDeg * wipS;
+			float tempi = radDeflectionAngle(sta, degi);
+			wipAdeg = degInRad * tempi;
+			//std::cout << "\n s=" << wipS << " a=" << wipAdeg << " targ=" << Adeg << " diff=" << abs(Adeg - wipAdeg); debug
+			if (abs(Adeg - wipAdeg) <= 0.0005) {
+				break;
+			}
+		}
+		degToRad(wipS);
+		return wipS;
+	}
+	void updateStationOblique(Station &sta, float S, float A) {
+		double sinS = sin(S);
+		double sinSsubA = sin(S - A);
+		float pM = sta.M;
+		double bM = (sta.gam - 1) * pow(pM, 2) * pow(sinS, 2) + 2;
+		double cM = 2 * sta.gam * pow(pM, 2) * pow(sinS, 2) - (sta.gam - 1);
+		sta.M = sqrt((bM / cM) / pow(sinSsubA, 2));
+
+		sta.P *= (2 * sta.gam * pow(pM, 2) * pow(sinS, 2) - (sta.gam - 1)) / (sta.gam + 1);
+		double gamPlusOne = sta.gam + 1;
+		sta.T *= ((2 * sta.gam * pow(pM, 2) * pow(sinS, 2) - (sta.gam - 1)) * ((sta.gam - 1) * pow(pM, 2) * pow(sinS, 2) + 2)) / (pow(gamPlusOne, 2) * pow(pM, 2) * pow(sinS, 2));
+		sta.rho *= ((sta.gam + 1) * pow(pM, 2) * pow(sinS, 2)) / ((sta.gam - 1) * pow(pM, 2) * pow(sinS, 2) + 2);
+		double aPt = ((sta.gam + 1) * pow(pM, 2) * pow(sinS, 2)) / ((sta.gam - 1) * pow(pM, 2) * pow(sinS, 2) + 2);
+		double bPt = sta.gam / (sta.gam - 1);
+		double cPt = (sta.gam + 1) / (2 * sta.gam * pow(pM, 2) * pow(sinS, 2) - (sta.gam - 1));
+		double dPt = 1 / (sta.gam - 1);
+		sta.Pt *= pow(aPt, bPt) * pow(cPt, dPt);
+		calImp::getGam(sta);
+		isen::getTt_fromT(sta);
+		calImp::getCp(sta);
+	}
 }
 
 namespace rayleigh {
