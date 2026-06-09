2d1ef9c9d661089f1f9c515cd3a5fefffad2f4a0
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2026-06-09 15:35:56 +1000

    Update formulaegg.h

--- aero/formulaegg.h
+++ aero/formulaegg.h
@@ -1,4 +1,42 @@
+// TABLE OF CONTENTS:
+// constants, physics stuff, unit conversion
+// fundamental structs such as Stations (air) and Shocks (types, angles)
+// calorically imperfect specific heat capacities
+// isentropic relations and the ideal gas law
+// normal shock relations
+// oblique shock relations
+// Rayleigh and Fanno flow
+
+// for complete solvers for shockwaves/combustion/etc see solvers.h
+
+// CONSTANTS //
+const float g = 9.80665;
+const float rEarth = 6356766;
+const float Runiv = 8314.46;
+const float molAir = 28.9644;
+const float RsAir = Runiv / molAir;
+const double e = exp(1.0);
+const double pi = 3.14159265359;
+const float Theta = 3055 + (5 / 9);
+const float gamDiatomic = 1.4;
+const float CpiAir = 1005;
+const double degInRad = 180 / pi;
+const double radInDeg = pi / 180;
+
+// UNIT CONVERSION //
+// probably mostly unused but good to have as a reference
+void degToRad(float &angle) {
+	angle *= radInDeg;
+}
+void radToDeg(float &angle) {
+	angle *= degInRad;
+}
+
+// FUNDAMENTAL STRUCTS AND ENUMS //
+// required for basically all the formulaegg/solvers void functions to work
+// updates: added position int and velocity parameter
 struct Station {
+	int pos = 0;
 	float M = 0;
 	float P = 101325;
 	float T = 288.15;
@@ -7,8 +45,36 @@ struct Station {
 	float Cp = 1005;
 	float Pt = 101325;
 	float Tt = 288.15;
+	float V = 0;
 };
 
+enum ShockType {
+	WEAK,
+	STRONG,
+	NORMAL,
+	DEBUG
+};
+
+// M decreases, P/T/rho increases, Pt decreases
+// (Tt should theoretically remain constant but I'm including some losses)
+struct Shock {
+	int pos = 0;
+	float degDeflection = 0.0;
+	float degShockwave = 0.0;
+	enum ShockType type = DEBUG;
+};
+
+// M increases, total conditions constant (isentropic)
+struct ExpansionFan {
+	int pos = 0;
+	float degDeflection = 0.0;
+	float degPrandtlMeyer = 0.0;
+};
+
+// CALORICALLY IMPERFECT SPECIFIC HEAT EQUATIONS //
+// for now all this stuff is based on ideal diatomic gas (gam=1.4)
+// will work well for air, but not for water vapour/carbon dioxide
+// heavy overhaul required at some point
 namespace calImp {
 	void getGam(Station &sta) {
 		double ThetaT = Theta / sta.T;	
@@ -24,8 +90,14 @@ namespace calImp {
 		double ThetaTsq = pow(ThetaT, 2);
 		sta.Cp = CpiAir * (1 + ((gamDiatomic - 1) / gamDiatomic) * (ThetaTsq * (exp(ThetaT) / ThetaTexpsub1sq)));
 	}
+	float getCv(Station &sta) {
+		double ThetaT = 0.0;
+		return ThetaT;
+	}
 }
 
+// ISENTROPIC RELATIONS //
+// ideal isentropic relations plus ideal gas law to find rho
 namespace isen {
 	void getTt_fromT(Station &sta) {
 		sta.Tt = sta.T * (1 + ((sta.gam - 1) / 2) * pow(sta.M, 2));
@@ -43,21 +115,64 @@ namespace isen {
 		double Ptow2 = sta.gam / (sta.gam - 1);
 		sta.P = sta.Pt / pow(Ptow1, Ptow2);
 	}
+	float getA_Astar(Station &sta) {
+		double a = (sta.gam + 1) / (2 * (sta.gam - 1));
+		double b = (sta.gam + 1) / 2;
+		double c = 1 + ((sta.gam - 1) / 2) * pow(sta.M, 2);
+		return pow(b, -a) * (pow(c, a) / sta.M);
+	}
+	void getVelocity(Station &sta) {
+		sta.V = sta.M * sqrt(RsAir * sta.gam * sta.T);
+	}
+	// this will require updating at the same time as calImp
+	// can't assume the station is constantly diatomic only with combustion products
+	void getRho_fromPT(Station &sta) {
+		sta.rho = sta.P / (RsAir * sta.T);
+	}
 }
 
+// NORMAL SHOCK RELATIONS //
 namespace normal {
+	void updateStationNormal(Station &sta) {
+		float pM = sta.M;
+		double aM = (sta.gam - 1) * pow(pM, 2) + 2;
+		double bM = 2 * sta.gam * pow(pM, 2) - (sta.gam - 1);
+		// it seems like I could probably optimise memory here:
+		// initialise as few doubles as possible at the start of functions as needed
+		// then use them as kinda virtual registers by updating and accessing them with pointers, and deleting once finished
+		// this doesn't seem like it'd work though, since stack memory is defined by scope
+		// might be a better idea to divide all this stuff up into inner functions or whatever, returning isn't that inefficient, i'm not duplicating very much data for long
+		// this is definitely something to do after trying to collect as many like terms as possible across all the necessary equations, rather than with the current unoptimised versions of them basically copied directly from formulaegg.py
+		sta.M = sqrt(aM / bM);
+		float Pr = (2 * sta.gam * pow(pM, 2) - (sta.gam - 1)) / (sta.gam + 1);
+		sta.P *= (2 * sta.gam * pow(pM, 2) - (sta.gam - 1)) / (sta.gam + 1);
+		float cT = sta.gam + 1; // no need for a double here
+		float Tr = ((2 * sta.gam * pow(pM, 2) - (sta.gam - 1)) * ((sta.gam - 1) * pow(pM, 2) + 2)) / (pow(cT, 2) * pow(pM, 2));
+		sta.T *= ((2 * sta.gam * pow(pM, 2) - (sta.gam - 1)) * ((sta.gam - 1) * pow(pM, 2) + 2)) / (pow(cT, 2) * pow(pM, 2));
+		float rhoR = ((sta.gam + 1) * pow(pM, 2)) / ((sta.gam - 1) * pow(pM, 2) + 2);
+		sta.rho *= ((sta.gam + 1) * pow(pM, 2)) / ((sta.gam - 1) * pow(pM, 2) + 2);
+		double aPt = ((sta.gam + 1) * pow(pM, 2)) / ((sta.gam - 1) * pow(pM, 2) + 2);
+		double bPt = (sta.gam + 1) / (2 * sta.gam * pow(pM, 2) - (sta.gam - 1));;
+		double cPt = sta.gam / (sta.gam - 1);
+		double dPt = 1 / (sta.gam - 1);
+		float Ptr = pow(aPt, cPt) * pow(bPt, dPt);
+		sta.Pt *= pow(aPt, cPt) * pow(bPt, -dPt);
+		std::cout << "\npM=" << pM << "  M=" << sta.M << " Pr=" << Pr << " Tr=" << Tr << " rhoR=" << rhoR << " Ptr=" << Ptr << " gam=" << sta.gam;
+		// just fuckin optimise later
+		calImp::getGam(sta);
+		isen::getTt_fromT(sta);
+		calImp::getCp(sta);
+		isen::getVelocity(sta);
+	}
 }
 
-const double degInRad = 180 / pi;
-const double radInDeg = pi / 180;
-void degToRad(float &angle) {
-	angle *= radInDeg;
-}
-void radToDeg(float &angle) {
-	angle *= degInRad;
-}
-
+// OBLIQUE SHOCK RELATIONS //
+// Mach angle, deflection from shock angle, maximum deflection before strong shock
+// basic shock from deflection angle solver (update with more efficient search algo one day
+// void function to update Station in a sensible order (apply oblique shock relations, find new gamma/Cp, find new total temperature
 namespace oblique {
+	// all these shock angle things are in radians since trig
+	// in the solver necessary unit conversions to degrees for the Shock struct update void func will be included
 	float radMachAngle(Station &sta) {
 		return asin(1 / sta.M);
 	}
@@ -102,6 +217,7 @@ namespace oblique {
 		degToRad(wipS);
 		return wipS;
 	}
+	// don't forget the conversions when calling this in the solver updater
 	void updateStationOblique(Station &sta, float S, float A) {
 		double sinS = sin(S);
 		double sinSsubA = sin(S - A);
@@ -122,8 +238,25 @@ namespace oblique {
 		calImp::getGam(sta);
 		isen::getTt_fromT(sta);
 		calImp::getCp(sta);
+		// check to make sure velocity update conserves mass
+		// or realistically yet pessimistically decreases it
+		// might account for necessary boundary layer bleed/losses
+		isen::getVelocity(sta);
 	}
 }
 
+// PRANDTL MEYER RELATIONS //
+// for expansion fans
+// will include basic solver and Station update void func
+namespace prandtlMeyer {
+}
+
+// RAYLEIGH FLOW RELATIONS //
+// for combustion (at some point)
 namespace rayleigh {
+}
+
+// FANNO FLOW RELATIONS //
+// for friction stuff, when I develop the necessary masochistic cajones
+namespace fanno {
 }
