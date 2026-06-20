da594cca5c2635a84cff5d7652ca5e4490dd11ec
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2026-06-20 16:40:16 +1000

    Update panel.c

--- src/panel.c
+++ src/panel.c
@@ -1,7 +1,7 @@
 #include "raylib.h"
 
 void drawPanel(char *ptrString) {
-	ClearBackground(BLACK);
-	DrawText(ptrString, 100, 100, 50, WHITE);
+	DrawText(ptrString, 50, 50, 50, (Color){245, 255, 245, 255});
+	DrawText("arrow keys for altitude/velocity\n[tab to reset]\nZ for fine control\nX for coarse", 50, 650, 25, (Color){255, 225, 225, 255});
 	DrawFPS(10, 10);
 }
