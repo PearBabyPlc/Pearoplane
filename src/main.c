98eabf0ac67a86c9b4fef2362b41152c828308a2
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2026-06-28 11:59:02 +1000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "resource_dir.h"
+#include "stdlib.h"
 #include "main.h"
 
 // default definitions
@@ -7,11 +8,16 @@
 #define DEFAULT_H 800
 
 int main() {
+	// initialise states
 	ScreenState screenState = {.screenW = DEFAULT_W, .screenH = DEFAULT_H, .currentScreen = 0};
 	ViewState outsideView = {.azimuth = 0.0, .elevation = 0.0, .zoom = 1.0};
 	ViewState orbitalView = {.azimuth = 0.0, .elevation = 0.0, .zoom = 1.0};
 	int keyPressed = -1;
 
+	// malloc for unimportant stuff like char buf[] arrays for deborging
+	char *printViewBuffer = malloc(70);
+
+	// Raylib window
 	InitWindow(DEFAULT_W, DEFAULT_H, "Pearoplane Alpha v0.1");
 	//SetTargetFPS(1200);
 	while (!WindowShouldClose()) {
@@ -27,14 +33,15 @@ int main() {
 		// draw screen depending on screenState
 		BeginDrawing();
 		if (screenState.currentScreen == 0) drawMenu();
-		if (screenState.currentScreen == 1) drawOutside(&outsideView);
-		if (screenState.currentScreen == 2) drawOrbital(&orbitalView);
+		if (screenState.currentScreen == 1) drawOutside(&outsideView, printViewBuffer);
+		if (screenState.currentScreen == 2) drawOrbital(&orbitalView, printViewBuffer);
 		if (screenState.currentScreen == 3) drawPanel();
 		if (screenState.currentScreen == 4) drawAero();
 		if (screenState.currentScreen == 5) drawProp();
 		EndDrawing();
 	}
-
 	CloseWindow();
+
+	free(printViewBuffer);
 	return 0;
 }
