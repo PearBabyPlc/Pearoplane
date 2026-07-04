a5a49e20daf08d33d570ed8d8764b6f5e0140292
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2026-07-04 11:39:34 +1000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 // this time around, minimum comparmentalisation
 // file size doesn't matter for modern computers
 // but function call overhead and duplication does
-// this version will have everything in one C file
+// this version will have each subprogram in a single C file
+// main.h will link everything
+// for now this is all gonna be testing out subprograms
+// such as the orbital model, 3d flight model, 3d rendering,
+// engine model, etc...
+// idfk what i'm doing
 
 // Raylib headers
 #include "raylib.h"
